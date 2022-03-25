925d63f67c91060da415663460d1c5d975814a30
CondoriM <condoria.miguel@gmail.com>
2022-03-25 17:45:10 +0100

    correction name

--- assets/main.js
+++ assets/main.js
@@ -2,6 +2,7 @@ function myFunction() {
     //costo del prezzo per km
     const price_km = 0.29
 
+    var user_name = document.getElementById("myName").value;
     var user_km = document.getElementById("myKm").value;
     var old_age = document.getElementById("myAge").value;
     console.log(old_age)
@@ -20,15 +21,18 @@ function myFunction() {
         return ((num/100) * 80).toFixed(2);
     }
 
+    //condizioni per lo sconto da erogare
     if (old_age == 'anziano'){
         var sconto = calcPerc(prezzo);
         document.getElementById('sct').innerHTML = sconto + '$';
         document.getElementById('sct_2').innerHTML = 'Non puo usufruire di questo sconto';
+        document.getElementById('name').innerHTML = user_name
     } else if (old_age == 'ragazzo') {
         var sconto_2 = calcPerc_20(prezzo);
         document.getElementById('sct_2').innerHTML = sconto_2 + '$';
         document.getElementById('sct').innerHTML = 'Non puo usufruire di questo sconto';
     } else {
-        document.getElementById('no_sct').innerHTML = prezzo + '$'
+        document.getElementById('no_sct').innerHTML = prezzo.toFixed(2) + '$'
     }
+
 }
